bed3475f74d8dc9533d14bfafb7c1477101e68ba
robert.brosh23 <robert.brosh23@gmail.com>
2026-02-01 19:42:04 -0500

    animate penguins dying

--- enemy/state/dead_state.gd
+++ enemy/state/dead_state.gd
@@ -1,12 +1,27 @@
 class_name DeadState
 extends EnemyBaseState
 
+@export var base_sprite: Sprite2D
+@export var animation_sprite: Sprite2D
+
 func enter() -> void:
 	super()
-	enemy.sprite.frame = 4
 	print("Dead state entered")
+	play_fall_animation()
 	await_get_up()
 	
+func play_fall_animation():
+	base_sprite.visible = false
+	animation_sprite.visible = true
+	animation_sprite.frame = 0
+	for i in range(1,5):
+		await get_tree().create_timer(.08).timeout
+		animation_sprite.frame += 1
+	
 func await_get_up() -> void:
-	await get_tree().create_timer(5.0).timeout
+	await get_tree().create_timer(3.0).timeout
+	animation_sprite.frame = 6
+	for i in range(6,12):
+		await get_tree().create_timer(.08).timeout
+		animation_sprite.frame += 1
 	Transitioned.emit(self, "RunAwayState")
--- enemy/state/run_away_state.gd
+++ enemy/state/run_away_state.gd
@@ -1,8 +1,35 @@
 class_name RunAwayState
 extends EnemyBaseState
 
+@export var base_sprite: Sprite2D
+@export var animation_sprite: Sprite2D
+
+var active := false
+
 func enter():
-	enemy.sprite.frame = 2
+	super()
+	active = true
+	start_animation()
+	
+func exit():
+	super()
+	animation_sprite.visible = false
+	active = false
 
 func physics_update(_delta: float):
 	enemy.global_position.x -= _delta * 40.0
+
+func start_animation():
+	base_sprite.visible = false
+	animation_sprite.visible = true
+	animation_sprite.frame = 14
+	while active:
+		await get_tree().create_timer(.1).timeout
+		if animation_sprite.frame == 17:
+			animation_sprite.frame = 14
+		else:
+			animation_sprite.frame += 1
+		
+func on_damage_taken():
+	print("I'm dead")
+	Transitioned.emit(self, "DeadState")
