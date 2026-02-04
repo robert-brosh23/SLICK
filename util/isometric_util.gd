b9151df07f219725991150230d058feb9ba29e1b
robert-brosh23 <75144595+robert-brosh23@users.noreply.github.com>
2026-02-04 11:33:58 -0500

    Update isometric_util.gd

--- util/isometric_util.gd
+++ util/isometric_util.gd
@@ -2,4 +2,6 @@ class_name IsometricUtil
 
 static func to_iso(to_convert: Vector2) -> Vector2:
 	return to_convert * Vector2(1.0, 0.5)
-	
+
+static func from_iso(to_convert: Vector2) -> Vector2:
+	return to_convert * Vector2(1.0, 2.0)
